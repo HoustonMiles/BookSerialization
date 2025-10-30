666c8b5d397a7713b3b425fdb39996a91b689325
HoustonMiles <mileshs1@g.cofc.edu>
2025-10-30 18:04:25 -0400

    Made binary serialization more like https://stackoverflow.com/questions/6683582/best-way-to-store-data-for-your-game-images-maps-and-such/6683609#6683609

--- src/main/java/com/example/BinarySerializer.java
+++ src/main/java/com/example/BinarySerializer.java
@@ -2,15 +2,37 @@ package com.example;
 import java.io.*;
 
 public class BinarySerializer {
-    public static void binarySerialize(Object obj, String file) throws IOException {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
-            out.writeObject(obj);
+    public static void binarySerialize(Object obj, String file) {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream(file));
+            oos.writeObject(obj);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.flush();
+                    oos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public static Object binaryDeserialize(String file) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
-            return in.readObject();
+    public static Object binaryDeserialize(String file) throws ClassNotFoundException {
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            return ois.readObject();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 }
--- src/test/java/com/example/BookTest.java
+++ src/test/java/com/example/BookTest.java
@@ -36,8 +36,8 @@ public class BookTest {
 
         // Serialize the original books to CSV
         //BookUtils.serializeToCSV(originalBooks, csvFile);
-        BookUtils.serializeToXML(originalBooks, xmlFile);
-        //BinarySerializer.binarySerialize(originalBooks, binaryFile);
+        //BookUtils.serializeToXML(originalBooks, xmlFile);
+        BinarySerializer.binarySerialize(originalBooks, binaryFile);
         for (Book book : originalBooks) {
             System.out.println(book.toString());
         }
@@ -45,8 +45,8 @@ public class BookTest {
         System.out.println("Test");
         // Deserialize the original books from CSV
         //Set<Book> deserializedBooks = BookUtils.deserializeFromXML(binaryFile);
-        Set<Book> deserializedBooks = BookUtils.deserializeFromXML(xmlFile);
-        //Set<Book> deserializedBooks = (Set<Book>) BinarySerializer.binaryDeserialize(binaryFile);
+        //Set<Book> deserializedBooks = BookUtils.deserializeFromXML(xmlFile);
+        Set<Book> deserializedBooks = (Set<Book>) BinarySerializer.binaryDeserialize(binaryFile);
         for (Book book : deserializedBooks) {
             System.out.println(book.toString());
         }
